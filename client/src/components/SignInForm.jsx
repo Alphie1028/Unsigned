9fc0ddb62862f9fe3b657f9ff937ee3751554a19
alphie1028 <alphie1028@gmail.com>
2023-04-02 11:25:19 -0400

    unstyled form for sign in, only console logs input

--- client/src/components/SignInForm.jsx
+++ client/src/components/SignInForm.jsx
@@ -1,12 +1,41 @@
 import React from "react";
+import { useState } from "react";
 
 function SignInForm() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(email, password);
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>Sign In</h2>
-            {/* add form fields here */}
+            <label>
+                Email:
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    required
+                />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    required
+                />
+            </label>
+            <br />
+            <button type="submit">Sign In</button>
         </form>
     );
 }
+
 
 export default SignInForm;
